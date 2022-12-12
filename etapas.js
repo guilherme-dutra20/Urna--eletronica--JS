0221b6938b9cb8e3608af9513ab63f135f95c8fc
guilherme-dutra20 <119949141+guilherme-dutra20@users.noreply.github.com>
2022-12-12 16:29:38 -0300

    Add files via upload

---
+++ etapas.js
@@ -0,0 +1,69 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '38111',
+                nome: 'Fulano de tal',
+                partido: 'ABC',
+                
+                fotos:[
+                    {url:'38111.jpg', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: '77222',
+                nome: 'Beltrano da Silva',
+                partido: 'DEFG',
+            
+                fotos:[
+                    {url:'77222.jpg', legenda: 'Vereador'}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '99',
+                nome: 'Louro José',
+                partido: 'JJJ',
+                vice: 'Cic',
+                fotos:[
+                    {url:'99.jpg', legenda: 'Prefeito'},
+                    {url:'99_2.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: '84',
+                nome: 'Zulano',
+                partido: 'QWERTY',
+                vice: 'Zul',
+                fotos:[
+                    {url:'84.jpg', legenda: 'Prefeito'},
+                    {url:'84_2.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PRESIDENTE',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '88',
+                nome: 'João Silva',
+                partido: 'JFH',
+                vice: 'Maria Silva',
+                fotos:[
+                    {url:'99.jpg', legenda: 'Presidente'},
+                    {url:'99_2.jpg', legenda: 'Vice-Presidente', small: true}
+                ]
+            }
+        ]
+
+    }
+];
